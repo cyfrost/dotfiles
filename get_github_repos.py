c85a66506cce84d2dbc095d4545fe563b93d284a
Cyrus Frost <cyrus.frost@hotmail.com>
2019-04-07 11:13:52 +0530

    fix bloody indentation

--- get_github_repos.py
+++ get_github_repos.py
@@ -5,17 +5,17 @@ import pygithub3
 GITHUB_OAUTH_TOKEN=""
 
 def main():
-    gh = pygithub3.Github(GITHUB_OAUTH_TOKEN)
-    repos_list = []
-    for repo in gh.get_user().get_repos():
-        uname=repo.owner.login
+	gh = pygithub3.Github(GITHUB_OAUTH_TOKEN)
+	repos_list = []
+	for repo in gh.get_user().get_repos():
+		uname=repo.owner.login
 		pwd=GITHUB_OAUTH_TOKEN
 		clone_url=repo.clone_url
 		index=clone_url.find('github.com')
 		auth_string = uname + ':' + pwd + '@'
 		authenticated_clone_url = clone_url[:index] + auth_string + clone_url[index:]
 		repos_list.append(authenticated_clone_url)
-    print(repos_list)
+	print(repos_list)
 
 if __name__ == '__main__':
-    main()
+	main()
