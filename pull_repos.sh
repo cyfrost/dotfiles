035473b55eb9911e77e9045cc950ff0a4ebfe44c
Cyrus Frost <cyrus.frost@hotmail.com>
2019-04-07 11:11:13 +0530

    Update pull_repos.sh

--- pull_repos.sh
+++ pull_repos.sh
@@ -5,24 +5,37 @@ REPOS_DIR="$HOME/repos"
 AUTH_TOKEN="$1"
 CUR_DIR=`pwd`
 
+# INSTALLL PYTHON SCRIPT DEPENDENCIES:
+#
+# pip install pygithub
+# pip install pygithub3
+#
+# This will work even for a Termux like environment where you can simply do `pkg install python && pip install pygithub pygithub3`
 #
 # https://raw.githubusercontent.com/cyfrost/dotfiles/master/get_github_repos.py
 #
-# #!/usr/bin/env python3
+##!/usr/bin/env python3
+#
+#import pygithub3
+#
+#GITHUB_OAUTH_TOKEN=""
+#
+#def main():
+#    gh = pygithub3.Github(GITHUB_OAUTH_TOKEN)
+#    repos_list = []
+#    for repo in gh.get_user().get_repos():
+#        uname=repo.owner.login
+#		pwd=GITHUB_OAUTH_TOKEN
+#		clone_url=repo.clone_url
+#		index=clone_url.find('github.com')
+#		auth_string = uname + ':' + pwd + '@'
+#		authenticated_clone_url = clone_url[:index] + auth_string + clone_url[index:]
+#		repos_list.append(authenticated_clone_url)
+#    print(repos_list)
+#
+#if __name__ == '__main__':
+#    main()
 
-# import pygithub3
-
-# GITHUB_OAUTH_TOKEN=""
-
-# def main():
-#     gh = pygithub3.Github(GITHUB_OAUTH_TOKEN)
-#     repos_list = []
-#     for repo in gh.get_user().get_repos():
-#         repos_list.append(repo.clone_url)
-#     print(repos_list)
-
-# if __name__ == '__main__':
-#     main()
 
 rm -f $GITHUB_REPOS_SCRIPT;
 
