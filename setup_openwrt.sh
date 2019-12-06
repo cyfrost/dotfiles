ee7b1d2f757411552468159640461134da25257e
Cyrus Frost <cyrus.frost@hotmail.com>
2019-12-06 07:06:43 +0530

    Update setup_openwrt.sh

--- setup_openwrt.sh
+++ setup_openwrt.sh
@@ -15,26 +15,58 @@ $ROOT_PASSWD
 EOF
 
 HOSTNAME="openwrt"
-echo 'Setting hostname to' $HOSTNAME
+echo 'Setting hostname to ' $HOSTNAME
 uci set system.@system[0].hostname="$HOSTNAME"
 uci commit system
 
-echo 'Updating software packages'
+echo 'Updating packages...'
 {
 	opkg update
 	opkg install luci luci-ssl tmux vim
 } > /dev/null
 
-echo 'Setting timezone info'
+echo 'Setting up few ash aliases...'
+cat <<EOT >> /etc/profile
+
+alias u='opkg update'
+alias ins='opkg install'
+alias no_chess='uci del dhcp.@dnsmasq[0].server; uci add_list dhcp.@dnsmasq[0].server=/chess.com/ && uci add_list dhcp.@dnsmasq[0].server=/lichess.org/ && uci commit dhcp && /etc/init.d/dnsmasq restart'
+alias chess='uci del dhcp.@dnsmasq[0].server && uci commit dhcp && /etc/init.d/dnsmasq restart'
+alias ~='cd ~'
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias c='reset'
+alias e='exit'
+alias rmd='rm -rf'
+alias j='jobs -l'
+alias ping='ping -c 5 google.com'
+alias mem='free -mlht'
+alias l='ls -AltchF --color=auto --group-directories-first'
+alias g='grep -i'
+alias v='vim'
+alias fs='df -hT'
+alias path='echo -e ${PATH//:/\\n}'
+alias ipe='curl -s ipinfo.io/ip'
+alias tn='tmux new-session -s '
+alias ta='tmux attach -t '
+alias tl='tmux ls '
+alias tk='tmux kill-session -s '
+alias ipl='ip -o -4 addr list $(ls /sys/class/net | grep enp) | awk "{print $4}" | cut -d/ -f1'
+
+EOT
+
+echo 'Setting timezone info...'
 uci set system.@system[0].zonename="$ZONENAME"
 uci set system.@system[0].timezone="$TIMEZONE"
 uci commit system
 
-echo 'Deleting default radios'
+echo 'Deleting default radios...'
 uci delete wireless.default_radio0
 uci delete wireless.default_radio1
 
-echo 'Setting up PPPoE on WAN intef'
+echo 'Setting up PPPoE on WAN intef...'
 uci add luci ifstate
 uci set luci.@ifstate[-1].interface='wan'
 uci set luci.@ifstate[-1].ifname='eth0'
@@ -47,7 +79,7 @@ uci set network.wan.ipv6='auto'
 uci commit network
 uci commit luci
 
-echo 'Staging a 5Ghz AP'
+echo 'Staging a 5Ghz AP...'
 uci set wireless.cyrus5="wifi-iface"
 uci set wireless.cyrus5.device='radio0'
 uci set wireless.cyrus5.mode='ap'
@@ -57,7 +89,7 @@ uci set wireless.cyrus5.ssid="$SSID_5GHZ"
 uci set wireless.cyrus5.encryption='psk2'
 uci set wireless.cyrus5.key="$AP_PASSWORD"
 
-echo 'Staging a 2.4Ghz AP'
+echo 'Staging a 2.4Ghz AP...'
 uci set wireless.cyrus="wifi-iface"
 uci set wireless.cyrus.device='radio1'
 uci set wireless.cyrus.mode='ap'
@@ -67,16 +99,16 @@ uci set wireless.cyrus.ssid="$SSID_2GHZ"
 uci set wireless.cyrus.encryption='psk2'
 uci set wireless.cyrus.key="$AP_PASSWORD"
 
-echo 'Disabling a few sites in network'
+echo 'Disabling a few sites in network...'
 uci add_list dhcp.@dnsmasq[0].server='/chess.com/'
 uci add_list dhcp.@dnsmasq[0].server='/lichess.org/'
 uci commit dhcp
 
-echo "Setting radios' locations"
+echo "Setting radios' locations..."
 uci set wireless.radio0.country="$RADIO_LOC_CODE"
 uci set wireless.radio1.country="$RADIO_LOC_CODE"
 
-echo 'Enabling radios'
+echo 'Enabling radios...'
 uci set wireless.radio0.disabled="0"
 uci set wireless.radio1.disabled="0"
 
@@ -84,4 +116,3 @@ uci commit wireless
 echo 'Configuration complete!'
 echo 'Rebooting...'
 reboot
-
